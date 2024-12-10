66d0ecadce19b58ae5ef8ba647b310134799e011
fidel <fhgalano@gmail.com>
2024-12-10 13:51:08 -0600

    solution for day 4 problem 1

---
+++ d4p1/src/main.rs
@@ -0,0 +1,126 @@
+use std::fs;
+use std::io::BufRead;
+use std::path::Path;
+use std::cmp::min;
+
+fn main() {
+    let input_path = Path::new("input");
+    let map = get_input_as_char_map(input_path);
+
+    let found_count = find(&"XMAS".to_string(), map);
+
+    dbg!(found_count);
+}
+
+fn get_input_as_char_map(path: &Path) -> Vec<Vec<char>> {
+    let mut map: Vec<Vec<char>> = Vec::new();
+
+    for d in fs::read(path).unwrap().lines() {
+        map.push(d.unwrap().chars().collect());
+    }
+
+    map
+}
+
+#[derive(Debug)]
+enum Direction {
+    U,
+    RU,
+    R,
+    RD,
+    D,
+    LD,
+    L,
+    LU,
+}
+
+impl Direction {
+    pub fn value(&self) -> [i32; 2] {
+        match *self {
+            Direction::U => [-1, 0],
+            Direction::RU => [-1, 1],
+            Direction::R => [0, 1],
+            Direction::RD => [1, 1],
+            Direction::D => [1, 0],
+            Direction::LD => [1, -1],
+            Direction::L => [0, -1],
+            Direction::LU => [-1, -1],
+        }
+    }
+
+    pub fn all() -> Vec<Self> {
+        vec![
+            Direction::U,
+            Direction::RU,
+            Direction::R,
+            Direction::RD,
+            Direction::D,
+            Direction::LD,
+            Direction::L,
+            Direction::LU,
+        ]
+    }
+}
+
+fn find(term: &String, map: Vec<Vec<char>>) -> i32 {
+    let mut count: i32 = 0;
+    let start: char = term.chars().next().unwrap();
+
+    for (y, row) in map.iter().enumerate() {
+        let mut start_pos: usize = 0;
+        while let Some(next_x) = position_from(start_pos, row, &start) {
+            for dir in Direction::all().iter() {
+                match search_direction(&term[0..].to_string(), [y as i32, next_x as i32], dir, &map) {
+                    true => count += 1,
+                    false => ()
+                };
+            }
+            start_pos = next_x + 1;
+            if start_pos >= row.len() {
+                break;
+            }
+            //println!("y - {:?}  x - {:?}  start - {:?}", y, next_x, start_pos);
+        }
+    }
+    count
+}
+
+fn position_from<T: std::cmp::PartialEq + std::fmt::Debug>(pos: usize, searchable: &Vec<T>, target: &T) -> Option<usize> {
+    match searchable[pos..].iter().position(|x| x == target) {
+        Some(n) => Some(pos + n),
+        _ => None
+    }
+}
+
+fn search_direction(
+    term: &String, 
+    coords: [i32;2], 
+    direction: &Direction, 
+    map: &Vec<Vec<char>>
+) -> bool {
+
+    if term.is_empty() {
+        return true
+    }
+
+    if coords[0] < 0 || coords[1] < 0 {
+        return false
+    }
+
+    if coords[0] >= map[0].len() as i32 || coords[1] >= map.len() as i32 {
+        return false
+    }
+
+    //println!("dir - {:?}  term - {:?}  stuff - {:?}", direction, term, map[coords[0] as usize][coords[1] as usize]);
+    match term.chars().next() {
+        Some(n) if n == map[coords[0] as usize][coords[1] as usize] => {
+            search_direction(
+                &term[1..].to_string(), 
+                [coords[0] + direction.value()[0], coords[1] + direction.value()[1]], 
+                direction, 
+                map,
+            )
+        },
+        _ => false,
+    }
+}
